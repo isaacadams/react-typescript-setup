1f8810f320967faea52676360ac0b3bc9c1c43b9
Isaac Adams <iadams@emoneyadvisor.com>
2018-11-12 12:01:37 -0500

    simplified gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,20 @@ let gulp = require('gulp'),
     fs = require("fs"),
     browserify = require("browserify"),
     tsify = require("tsify"),
-    buffer = require('vinyl-buffer'),
-    source = require('vinyl-source-stream'),
     babelify = require('babelify');
 
 let app = {
     //entry extension can be .jsx, .js, .ts, or .tsx
     entry: './source/scripts/main.tsx',
-    publish_filename: 'publish.js',
-    publish_directory: './dist/scripts',
+    publish: './dist/scripts/publish.js',
     tsconfig: './tsconfig.json'
 };
 
 gulp.task('app.build', function () {
 
     let b = browserify({
-            entries: [app.entry]
-        });
+        entries: [app.entry]
+    });
 
     b.plugin(tsify, getJsonFile(app.tsconfig).compilerOptions);
     b.transform(babelify,
@@ -29,9 +26,7 @@ gulp.task('app.build', function () {
     
     console.log('Building...');
     return b.bundle()
-        .pipe(source(app.publish_filename))
-        .pipe(buffer())
-        .pipe(gulp.dest(app.publish_directory));
+        .pipe(fs.createWriteStream(app.publish));
 });
 
 function getJsonFile(path) {
